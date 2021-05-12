95fca4d1ea6029f1006f36e9ad349655b4e94231
Michael Felix <60325397+mifelix@users.noreply.github.com>
2021-05-12 16:57:32 -0600

    Add files via upload

---
+++ Pointers_Example_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+char shahnaz, mahnaz;
+char * p =&shahnaz;
+char * q =&mahnaz ;
+*p = 'h';
+*q=*(++p);
+cout <<"p="<<*p<<endl;
+cout <<"shahnaz="<<shahnaz<<endl;
+cout <<"q="<<*q<<endl;
+return 0; }
+
+//Output: 
+//p=
+//shahnaz=h
+//q=
